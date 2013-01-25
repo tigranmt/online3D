f795c7509d5ec80e8159b1921b2d65e7957f3a80
Tigran Martirosyan <tigranmt@gmail.com>
2013-01-25 20:38:55 +0100

    Fix on models loading
    Do not make any restriciton on user name

--- online3D/Controllers/StlController.cs
+++ online3D/Controllers/StlController.cs
@@ -44,14 +44,16 @@ namespace ModelViewer3D.Controllers
         {
             try
             {
-                var userName = GetUserName();
+                /*var userName = GetUserName();
                 MongoDataAccess access = new MongoDataAccess();
                 var models = access.ReadModelCollection(id, false).Where(m => m.User == userName);
                 
                 //if on current user failed, try search among sample models
                 if(models.Count() == 0)
-                    models = access.ReadModelCollection(id, false).Where(m => m.User == "sample");
+                    models = access.ReadModelCollection(id, false).Where(m => m.User == "sample");*/
 
+                MongoDataAccess access = new MongoDataAccess();
+                var models = access.ReadModelCollection(id, false);
                 ViewBag.ID = models.First().ID;
             }
             catch(Exception ex)
