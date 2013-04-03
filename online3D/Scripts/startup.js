73bc216b0e28f94be5ec2b6178ab17053401196f
tigranmt <tigranmt@gmail.com>
2013-04-03 17:23:24 +0200

    some fixes on DropBox API

--- online3D/Scripts/startup.js
+++ online3D/Scripts/startup.js
@@ -49,20 +49,34 @@
 
             success: function (files) {
 
+
                 console.log(files);
                 var appKey = { key: '254hrzt7wfhcmxv', secret: 'i9f2dtdx5y67wui', sandbox: true };
                 var client = new Dropbox.Client(appKey);
 
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i];
-                    client.readFile(file.name, function (error, data) {
-                        if (error) {
-                            return console.log(error);  // Something went wrong.
-                        }
 
-                        alert(data);  // data has the file's contents
-                    });
-                }
+                client.authenticate(function (error, client) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    if (client.isAuthenticated()) {
+                        // var read_options = {arrayBuffer: true};
+                        for (var i = 0; i < files.length; i++) {
+                            var file = files[i];
+                            client.readFile(file.link, function (error, data) {
+                                if (error) {
+                                    return console.log(error);  // Something went wrong.
+                                }
+
+                                alert(data);  // data has the file's contents
+                            });
+                        }
+                    } else {
+                        console.log("Error on authentication");
+                    }
+                });
+
+               
             },
             cancel: function () {
 
